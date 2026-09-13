9ab6392d6a13a0a830e2834518bc6f82b969b1c9
Mainak Das <mainakdas104@gmail.com>
2026-09-13 15:00:32 +0530

    feat: implement Post model with SQLAlchemy mappings

---
+++ app/db/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Boolean, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .database import Base
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    # id = Column(Integer, primary_key=True, nullable=False)
+    # title = Column(String, nullable=False)
+    # content = Column(String, nullable=False)
+    # published = Column(Boolean, default=True)\
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String)
+    content: Mapped[str] = mapped_column(String)
+    published: Mapped[bool] = mapped_column(Boolean, default=True)
