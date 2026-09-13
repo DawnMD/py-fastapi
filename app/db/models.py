48c53f8b3825922c6345598aa1201c2021c62d39
Mainak Das <mainakdas104@gmail.com>
2026-09-13 16:03:50 +0530

    refactor: adjust Post model fields for improved initialization and defaults

--- app/db/models.py
+++ app/db/models.py
@@ -12,13 +12,17 @@ class Post(Base):
     # id = Column(Integer, primary_key=True, nullable=False)
     # title = Column(String, nullable=False)
     # content = Column(String, nullable=False)
-    # published = Column(Boolean, default=True)\
-    id: Mapped[int] = mapped_column(primary_key=True)
+    # published = Column(Boolean, default=True)
+
+    # init for telling the orm dont provide during initialisation
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
     title: Mapped[str] = mapped_column(String)
     content: Mapped[str] = mapped_column(String)
     # need to place server default as we need to maintain it from server
-    published: Mapped[bool] = mapped_column(Boolean, server_default="true")
-
+    # default=True for making it optional
+    published: Mapped[bool] = mapped_column(
+        Boolean, server_default="true", default=True
+    )
     created_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), server_default=func.now()
+        DateTime(timezone=True), server_default=func.now(), init=False
     )
