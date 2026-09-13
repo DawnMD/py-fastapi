99e029d4f404d62163854a49621d8106c5bcf290
Mainak Das <mainakdas104@gmail.com>
2026-09-13 15:00:26 +0530

    feat: add database module with SQLAlchemy setup and session management

---
+++ app/db/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, Session
+
+DB_URL = "postgresql://postgres:admin@localhost:5432/pyfastapi"
+
+engine = create_engine(DB_URL, echo=True)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    with Session(engine, autocommit=False, autoflush=False) as session:
+        yield session
