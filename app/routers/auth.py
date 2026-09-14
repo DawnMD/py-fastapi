4cb4039b9094ead71079fed383bbe27892738405
Mainak Das <mainakdas104@gmail.com>
2026-09-14 13:05:45 +0530

    feat: update login endpoint to use Token model and correct HTTP status code

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 
 from app.dependencies import DbSession
-from app.utils import Token, check_user_creds, create_access_token
+from app.oauth2.utils import Token, check_user_creds, create_access_token
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 ph = PasswordHasher()
@@ -16,8 +16,8 @@ def login(user: Annotated[OAuth2PasswordRequestForm, Depends()], db: DbSession):
     valid_user = check_user_creds(username=user.username, password=user.password, db=db)
 
     if not valid_user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
 
     token = create_access_token({"email": valid_user.email, "id": valid_user.id})
 
-    return {"access_token": token, "token_type": "Bearer"}
+    return Token(access_token=token, token_type="Bearer")
