a33b60cbbb659341bc2d468b965a54f61d89388f
Mainak Das <mainakdas104@gmail.com>
2026-09-14 19:44:07 +0530

    feat: update Post and User models to include user relationship and fix email type

--- app/db/models.py
+++ app/db/models.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 
-from pydantic import EmailStr
-from sqlalchemy import Boolean, DateTime, String, func
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Boolean, DateTime, ForeignKey, String, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .database import Base
 
@@ -21,6 +20,13 @@ class Post(Base):
     content: Mapped[str] = mapped_column(String)
     # need to place server default as we need to maintain it from server
     # default=True for making it optional
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+    user: Mapped[User] = relationship(
+        back_populates="posts",
+        init=False,
+    )
     published: Mapped[bool] = mapped_column(
         Boolean, server_default="true", default=True
     )
@@ -33,8 +39,14 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True, init=False)
-    email: Mapped[EmailStr] = mapped_column(String, unique=True)
+    email: Mapped[str] = mapped_column(String, unique=True)
     password: Mapped[str] = mapped_column(String)
+    posts: Mapped[list[Post]] = relationship(
+        back_populates="user",
+        cascade="all, delete-orphan",
+        passive_deletes=True,
+        init=False,
+    )
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), init=False
     )
