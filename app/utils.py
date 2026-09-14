37dd264d485231fe52bd6acbd0b47947f5fddebe
Mainak Das <mainakdas104@gmail.com>
2026-09-14 12:34:33 +0530

    feat: add Token model for access token structure

--- app/utils.py
+++ app/utils.py
@@ -2,16 +2,22 @@ from datetime import UTC, datetime, timedelta
 from typing import Any
 
 import jwt
+from pydantic import BaseModel
 
 SECRET_KEY = "e3d17ccb64a6e0338fc11537a3a0032d8f9a64557cd1915bbd43dec2e710b135"
 EXPIRE_IN_MINS = 30
 ALGORITHM = "HS256"
 
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
 def create_access_token(data: dict[str, Any]) -> str:
     to_encode = data.copy()
 
-    expire = datetime.now(tz=UTC) + timedelta(minutes=30)
+    expire = datetime.now(UTC) + timedelta(minutes=30)
 
     to_encode["exp"] = expire
 
