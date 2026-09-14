deb85d42df03e27329f13c1ef6de12e447177884
Mainak Das <mainakdas104@gmail.com>
2026-09-14 13:05:38 +0530

    feat: move OAuth2 utility functions to a new utils.py file and remove old utils.py

---
+++ app/oauth2/utils.py
@@ -0,0 +1,89 @@
+from datetime import UTC, datetime, timedelta
+from typing import Annotated, Any
+
+import jwt
+from argon2 import PasswordHasher
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel, EmailStr, ValidationError
+from sqlalchemy import select
+
+from app.db.models import User
+from app.dependencies import DbSession
+
+ph = PasswordHasher()
+
+SECRET_KEY = "e3d17ccb64a6e0338fc11537a3a0032d8f9a64557cd1915bbd43dec2e710b135"
+EXPIRE_IN_MINS = 30
+ALGORITHM = "HS256"
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    id: int | None = None
+    email: EmailStr | None = None
+
+
+def create_access_token(data: dict[str, Any]) -> str:
+    to_encode = data.copy()
+
+    expire = datetime.now(UTC) + timedelta(minutes=30)
+
+    to_encode["exp"] = expire
+
+    return jwt.encode(  # type: ignore
+        payload=to_encode,
+        key=SECRET_KEY,
+        algorithm=ALGORITHM,
+    )
+
+
+def check_user_creds(db: DbSession, username: str, password: str):
+    statement = select(User).where(User.email == username)
+    db_user = db.scalar(statement)
+
+    if not db_user:
+        return False
+
+    verify = ph.verify(password=password, hash=db_user.password)
+
+    if not verify:
+        return False
+
+    return db_user
+
+
+def verify_token(token: str, exception: HTTPException):
+    try:
+        payload = jwt.decode(  # type: ignore
+            jwt=token,
+            key=SECRET_KEY,
+            algorithms=[ALGORITHM],
+        )
+
+        user_id = payload.get("id")
+        email = payload.get("email")
+
+        if user_id is None or email is None:
+            raise exception
+
+        return TokenData(id=user_id, email=email)
+
+    except jwt.InvalidTokenError, ValidationError:
+        raise exception
+
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    return verify_token(token=token, exception=credentials_exception)
