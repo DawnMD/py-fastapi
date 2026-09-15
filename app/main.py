9a4b0685b5e99454386c73808e4efdae88c5f5ab
Mainak Das <mainakdas104@gmail.com>
2026-09-15 15:58:32 +0530

    fix: comment out table creation in main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
 
-from app.db.database import Base, engine
 from app.routers import auth, post, user, vote
 
 app = FastAPI()
 
 # To create the tables/ models from code
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
 
 app.include_router(user.router)
 app.include_router(post.router)
