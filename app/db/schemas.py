c42a0d410e555fe07ba4db6d41a0b9985a317efe
Mainak Das <mainakdas104@gmail.com>
2026-09-15 15:09:44 +0530

    fix: remove unused created_at field from Post schema

--- app/db/schemas.py
+++ app/db/schemas.py
@@ -1,5 +1,4 @@
 # need to make a type class as we cannot use sqlalchemy class directly as type
-from datetime import datetime
 
 from pydantic import BaseModel, EmailStr
 
@@ -8,7 +7,6 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    created_at: datetime
 
 
 class PostResponse(Post):
